475fcaa2e9b8ea08307f9668dfb4f369a16a6999
ntkwan <npquan2704@gmail.com>
2025-04-29 11:45:21 +0000

    refactor: change api name

--- backend/src/articles/articles.controller.ts
+++ backend/src/articles/articles.controller.ts
@@ -1,15 +1,10 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import {
-    ArticlesService,
-    Article,
-    PaginatedArticlesResult,
-} from './articles.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { DateRangeDto, DateRangePaginationDto } from './dtos/time-range.dto';
+import { ArticlesService, PaginatedArticlesResult } from './articles.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { DateRangePaginationDto } from './dtos/time-range.dto';
 import { PaginationDto } from './dtos/pagination.dto';
 import { PaginatedArticlesResponseDto } from './dtos/pagination-response.dto';
 
-@ApiTags('articles')
 @Controller('articles')
 export class ArticlesController {
     constructor(private readonly articlesService: ArticlesService) {}
@@ -29,40 +24,13 @@ export class ArticlesController {
             paginationDto.pageSize,
         );
     }
-
-    @ApiOperation({ summary: 'Get articles by date range' })
-    @ApiResponse({
-        status: 200,
-        description: 'Returns articles filtered by date range',
-        schema: {
-            type: 'array',
-            items: {
-                type: 'object',
-                properties: {
-                    url: { type: 'string' },
-                    title: { type: 'string' },
-                    content: { type: 'string' },
-                },
-            },
-        },
-    })
-    @Get('filter')
-    async getArticlesByDateRange(
-        @Query() dateRange: DateRangeDto,
-    ): Promise<Article[]> {
-        return this.articlesService.getArticlesBetweenDates(
-            dateRange.startTime,
-            dateRange.endTime,
-        );
-    }
-
     @ApiOperation({ summary: 'Get articles by date range with pagination' })
     @ApiResponse({
         status: 200,
         description: 'Returns paginated articles filtered by date range',
         type: PaginatedArticlesResponseDto,
     })
-    @Get('filter/paginated')
+    @Get('time')
     async getArticlesByDateRangePaginated(
         @Query() query: DateRangePaginationDto,
     ): Promise<PaginatedArticlesResult> {
