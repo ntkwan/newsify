b1b4db47900b47cf9db2510c56c5fc087238f94b
ntkwan <npquan2704@gmail.com>
2025-05-04 21:20:49 +0000

    chore: disable https enforcement

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,7 +8,24 @@ const nextConfig: NextConfig = {
                 protocol: 'https',
                 hostname: '**',
             },
+            {
+                protocol: 'http',  
+                hostname: '**',
+            },
         ],
+    },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' http: https: data:; font-src 'self'; connect-src 'self' http: https:;",
+                    },
+                ],
+            },
+        ];
     },
     webpack(config) {
         config.module.rules.push({
